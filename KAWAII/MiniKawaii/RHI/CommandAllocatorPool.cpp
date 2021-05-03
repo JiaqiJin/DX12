2c5f71a876aaaa76c40fb945fc1cc9c6b2a06d20
kawakawaDe <fdsam@hotmail.com>
2021-05-03 21:19:30 +0100

    update

--- KAWAII/MiniKawaii/RHI/CommandAllocatorPool.cpp
+++ KAWAII/MiniKawaii/RHI/CommandAllocatorPool.cpp
@@ -10,9 +10,31 @@ namespace RHI
 
 	}
 
-	ID3D12CommandAllocator* CommandAllocatorPool::RequestAllocator(UINT64 CompletedFenceValue)
+	ID3D12CommandAllocator* CommandAllocatorPool::RequestAllocator(UINT64 completedFenceValue)
 	{
 		ID3D12CommandAllocator* pAllocator = nullptr;
+
+		if (!m_ReadyAllocators.empty())
+		{
+			std::pair<uint64_t, ID3D12CommandAllocator*>& AllocatorPair = m_ReadyAllocators.front();
+
+			if (AllocatorPair.first <= completedFenceValue)
+			{
+				pAllocator = AllocatorPair.second;
+				ThrowIfFailed(pAllocator->Reset());
+				m_ReadyAllocators.pop();
+			}
+		}
+
+		// If no allocator's were ready to be reused, create a new one
+		if (pAllocator == nullptr)
+		{
+			ThrowIfFailed(m_Device->CreateCommandAllocator(m_cCommandListType, IID_PPV_ARGS(&pAllocator)));
+			wchar_t allocatorName[32];
+			swprintf(allocatorName, 32, L"CommandAllocator %zu", m_Allocators.size());
+			pAllocator->SetName(allocatorName);
+			m_Allocators.push_back(pAllocator);
+		}
 
 		return pAllocator;
 	}
--- KAWAII/MiniKawaii/RHI/DescriptorHeap.h
+++ KAWAII/MiniKawaii/RHI/DescriptorHeap.h
@@ -1,11 +1,31 @@
 #pragma once
+/*Descriptor are structure which tell shader where to find the resource, and how interprete data resource.
+* Descriptor heap are chunk of memory where descriptro are stored. Shader Visible(CBV, UAV, Sampler, SRV), 
+* Shader No Visible(RTV, DSV, IBV, VBV, and SOV) 
+*/
 
 namespace RHI
 {
+	class RenderDevice;
+	
 	/*
-	* Descriptor are structure which tell shader where to find the resource, and how interprete data resource.
-	* Descriptor heap are chunk of memory where descriptro are stored. Shader Visible(CBV, UAV, Sampler, SRV), Shader No Visible(RTV, DSV, IBV, VBV, and SOV)
-	* 
+	* Descripe the allocation of the descriptors
 	*/
+	class DescriptorHeapAllocation
+	{
+	public:
+		// Creates null allocation
+		DescriptorHeapAllocation() noexcept :
+			m_NumHandles{ 1 },
+			m_pDescriptorHeap{ nullptr }
+		{
 
+		}
+
+	private:
+		D3D12_CPU_DESCRIPTOR_HANDLE m_FirstCpuHandle = { 0 };
+		D3D12_GPU_DESCRIPTOR_HANDLE m_FirstGpuHandle = { 0 };
+		ID3D12DescriptorHeap* m_pDescriptorHeap = nullptr;
+		UINT32 m_NumHandles = 0;
+	};
 }
