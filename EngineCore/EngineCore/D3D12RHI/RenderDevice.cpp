6c3d485312635bc303116f66a2625e286f581006
kawakawaDe <fdsam@hotmail.com>
2021-06-16 12:47:38 +0100

    device

--- EngineCore/EngineCore/D3D12RHI/RenderDevice.cpp
+++ EngineCore/EngineCore/D3D12RHI/RenderDevice.cpp
@@ -1,2 +1,38 @@
 #include "../pch.h"
 #include "RenderDevice.h"
+
+
+namespace RHI
+{
+	RenderDevice::RenderDevice(ID3D12Device* d3d12Device)
+		: m_D3D12Device(d3d12Device)
+	{
+		// TODO
+	}
+
+	RenderDevice::~RenderDevice()
+	{
+		// todo
+	}
+
+	void RenderDevice::PurgeReleaseQueue(bool forceRelease)
+	{
+		UINT64 graphicCompletedFenceValue = CommandListManager::GetSingleton().GetGraphicsQueue().GetCompletedFenceValue();
+
+		if (forceRelease)
+		{
+			graphicCompletedFenceValue = std::numeric_limits<UINT64>::max();
+			//computeCompletedFenceValue = std::numeric_limits<UINT64>::max();
+			//copyCompletedFenceValue = std::numeric_limits<UINT64>::max();
+		}
+
+		while (!m_ReleaseQueue.empty())
+		{
+			auto& firstObj = m_ReleaseQueue.front();
+			if (std::get<0>(firstObj) <= graphicCompletedFenceValue)
+				m_ReleaseQueue.pop_front();
+			else
+				break;
+		}
+	}
+}
