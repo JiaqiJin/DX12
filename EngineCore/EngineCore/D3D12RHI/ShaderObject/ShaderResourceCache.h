bee3c419e8574d6701b06c8654877c5deaa760b7
kawakawaDe <fdsam@hotmail.com>
2021-07-08 11:53:36 +0100

    update shader cache

--- EngineCore/EngineCore/D3D12RHI/ShaderObject/ShaderResourceCache.h
+++ EngineCore/EngineCore/D3D12RHI/ShaderObject/ShaderResourceCache.h
@@ -56,6 +56,35 @@ namespace RHI
             std::vector<std::shared_ptr<GpuResourceDescriptor>> Descriptors;
         };
 
+        // ShaderResourceLayout obtains the "Descriptor Handle" through this function, and copies the Descriptor of the resource to be bound
+        // OffsetFromTableStart is the offset in the Table, which is different from RootParam.m_TableStartOffset
+        template <D3D12_DESCRIPTOR_HEAP_TYPE HeapType>
+        D3D12_CPU_DESCRIPTOR_HANDLE GetShaderVisibleTableCPUDescriptorHandle(UINT32 RootIndex, UINT32 OffsetFromTableStart = 0) const
+        {
+            const auto& RootParam = GetRootTable(RootIndex);
+
+            D3D12_CPU_DESCRIPTOR_HANDLE CPUDescriptorHandle = { 0 };
+
+            if (RootParam.TableStartOffset != InvalidDescriptorOffset)
+                CPUDescriptorHandle = m_CbvSrvUavGPUHeapSpace.GetCpuHandle(RootParam.TableStartOffset + OffsetFromTableStart);
+
+            return CPUDescriptorHandle;
+        }
+
+        // RootSignature accesses the GPU Descriptor Handle through this function, and then submits it to the rendering pipeline
+        // OffsetFromTableStart is the offset in the Table, which is different from RootParam.m_TableStartOffset
+        template <D3D12_DESCRIPTOR_HEAP_TYPE HeapType>
+        D3D12_GPU_DESCRIPTOR_HANDLE GetShaderVisibleTableGPUDescriptorHandle(UINT32 RootIndex, UINT32 OffsetFromTableStart = 0) const
+        {
+            const auto& RootParam = GetRootTable(RootIndex);
+
+            assert(RootParam.TableStartOffset != InvalidDescriptorOffset && "GPU descriptor handle must never be requested for dynamic resources");
+
+            D3D12_GPU_DESCRIPTOR_HANDLE GPUDescriptorHandle = m_CbvSrvUavGPUHeapSpace.GetGpuHandle(RootParam.TableStartOffset + OffsetFromTableStart);
+
+            return GPUDescriptorHandle;
+        }
+
         RootDescriptor& GetRootDescriptor(UINT32 RootIndex)
         {
             return m_RootDescriptors.at(RootIndex);
